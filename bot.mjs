8c982ddfc0de79055fca46c1b91ec7e98d4eda9d
Monstrello <121196060+Monstrello@users.noreply.github.com>
2023-02-22 11:25:25 +0300

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -4,4 +4,6 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on('text', msg => msg.reply.text(msg.text))
 
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Банжорно Бамбини>'));
+
 export default bot
