845dbfc9300a5434686385e9252242bc66ea234f
Monstrello <121196060+Monstrello@users.noreply.github.com>
2023-02-22 12:38:07 +0300

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -1,6 +1,6 @@
 import TeleBot from "telebot"
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.5610303721:AAFOAKOR5XyZrNi7dXFdjJ5xbKRFyVdc_w8)
 
 bot.on('text', msg => msg.reply.text(msg.text))
 
